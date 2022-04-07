1928b78a326590aa2134d21227b0b9ca9fd602da
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-04-07 10:48:52 +0530

    Update Program1.cpp

--- Practical/week4/week5/Program1.cpp
+++ Practical/week4/week5/Program1.cpp
@@ -1,3 +1,6 @@
+/*Given an unsorted array of alphabets containing duplicate elements. Design an algorithm and
+implement it using a program to find which alphabet has maximum number of occurrences and
+print it. (Time Complexity = O(n)) */
 #include <iostream>
 #include <vector>
 using namespace std;

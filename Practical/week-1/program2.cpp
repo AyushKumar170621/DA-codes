608e048faa909f019020fc02de481bcd2e9dc355
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-05-05 10:35:58 +0530

    Update program2.cpp

--- Practical/week-1/program2.cpp
+++ Practical/week-1/program2.cpp
@@ -1,28 +1,13 @@
 /*Given an already sorted array of +ve integers disign an algorithm and implement it using program to find wheather the key is present or not also find the number
 of comparsion*/
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int bin_search(int a[],int l,int h,int key,int *c)
-{
-   if(l <=h) 
-    {int mid=(l+h)/2;
-    (*c)++;
-    if(key == a[mid])
-    {
-        return mid;
-    }
-    if(key > a[mid])
-        return bin_search(a,mid+1,h,key,&(*c));
-    else if(key < a[mid])
-        return bin_search(a,l,mid-1,key,&(*c));
- }
-    return -1;
-}
-int bin_lsearch(int a[],int l,int h,int key,int *c)
 {
     while(l <= h)
     {
         int mid=(l+h)/2;
-        (*c)++;
+        ++(*c);
         if(key == a[mid])
         {
             return mid;
@@ -36,25 +21,27 @@ int bin_lsearch(int a[],int l,int h,int key,int *c)
 }
 int main()
 {
-    int n,key,c=0;
-    printf("Enter size of array :");
-    scanf("%d",&n);
-    int a[n];
-    printf("Enter elements in array :");
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("Enter key to search :");
-    scanf("%d",&key);
-    int ind=bin_search(a,0,n,key,&c);
-    if(ind == -1)
-    {
-        cout<<"Not Present"<<endl;
-    }
-    else
-    {
-        cout<<"Present  "<<ind<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,key,c=0;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        cin>>key;
+        int ind=bin_search(a,0,n-1,key,&c);
+        if(ind == -1)
+        {
+            cout<<"Not Present"<<endl;
+        }
+        else
+        {
+            cout<<"Present  "<<c<<endl;
+        }
     }
     return 0;
 }
+

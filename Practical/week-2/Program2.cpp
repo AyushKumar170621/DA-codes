f8d66dafd301aa69f3f115802d1c0d767696ebc6
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-05-05 10:39:09 +0530

    Update Program2.cpp

--- Practical/week-2/Program2.cpp
+++ Practical/week-2/Program2.cpp
@@ -1,28 +1,34 @@
-/*3) Given a array of non negitive integers design a algo and implement it using a prog to count the no of pairs of 
-integers such that their differences is equal to the given key */
-#include <iostream>
-
+/*2.Given a sorted array of positive integers, design an algorithm and implement it using a program to find three indices i, j, k such that arr[i] + arr[j] = arr[k].
+Source code :-*/
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int n,key,c=0;
-    cout<<"Enter size of array :";
-    cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)
-        cin>>a[i];
-    cout<<"Enter key :";
-    cin>>key;
-    int i,j,k;
-    for(i=0;i<n-1;i++)
-    {
-        for(j=i+1 ;j < n ;j++)
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        int i,j,k,f=0;
+        for(i=0;i<n-2;i++)
         {
-            if(abs(a[i] - a[j])==key)
-                c++;
+            for(j=i+1 ;j < n-1 ;j++)
+            {
+                for(k=j+1 ; k<n ;k++)
+                {
+                    if(a[i]+a[j] == a[k]){
+                        cout<<i+1<<" "<<j+1<<" "<<k+1<<endl;
+                        f=1;
+                    }
+                }
+            }
         }
+        if(f==0)
+            cout<<"No Sequence Found"<<endl;
     }
-    cout<<"Number of pair having difference equal to "<<key<<"are "<<c<<endl;
     return 0;
 }

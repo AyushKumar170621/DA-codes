2813e34180a02734b83b60cb9d6e4d90c55d17f6
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-05-12 10:22:13 +0530

    Update Program3.cpp

--- Practical/week 3/Program3.cpp
+++ Practical/week 3/Program3.cpp
@@ -1,3 +1,5 @@
+3. Given an unsorted array of positive integers, design an algorithm and implement it using a program to find whether there are any duplicate elements in the array or not.
+Source code :-
 #include <iostream>
 
 using namespace std;
@@ -8,40 +10,80 @@ void readArr(int a[],int n)
         cin>>a[i];
     }
 }
-void printArr(int a[],int n)
+void merge(int arr[], int l, int m, int h)
 {
-    for(int i = 0 ; i < n ; i++)
+    int n1=m-l+1,n2=h-m;
+    int a1[n1];
+    int a2[n2];
+    for(int i=l,j=0;i<=m;i++,j++)
     {
-        cout<<a[i]<<" ";
+        a1[j]=arr[i];
     }
-    cout<<endl;
-}
-int insertionSort(int a[],int n)
-{
-    int cnt=0;
-    for(int i=1 ;i < n ;i++)
+    for(int i=m+1,j=0;i<=h;i++,j++)
     {
-        int key = a[i];
-        int j= i -1 ;
-        while(j >= 0 && key < a[j] )
+        a2[j]=arr[i];
+    }
+    int i=0,j=0,k=l;
+    while(i < n1 && j < n2)
+    {
+        if(a1[i] < a2[j])
         {
-            cnt++;
-            a[j+1]=a[j];
-            j--;
+            arr[k]=a1[i];
+            i++;
         }
-        a[j+1]=key;
+        else
+        {
+            arr[k]=a2[j];
+            j++;
+        }
+        k++;
     }
-    return cnt;
+    while(i < n1)
+    {
+        arr[k]=a1[i];
+        i++;
+        k++;  
+    }
+    while(j < n2)
+    {
+        arr[k]=a2[j];
+        j++;
+        k++;  
+    }
+}
+void mergeSort(int arr[],int l,int h)
+{
+    if(l==h)
+        return;
+    else
+    {
+        int mid=(l+h)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
 }
 int main()
 {
-    int n;
-    cout<<"Enter no of element of array :";
-    cin >> n;
-    int a[n];
-    readArr(a,n);
-    int com = insertionSort(a,n);
-    printArr(a,n);
-    cout<<"Total no of Comparison is "<<com<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,f=0;
+        cin >> n;
+        int a[n];
+        readArr(a,n);
+        mergeSort(a,0,n-1);
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==a[i-1])
+            {
+                f=1;
+                cout<<"Yes"<<endl;
+                break;
+            }
+        }
+        if(f==0)
+            cout<<"No"<<endl;
+    }
     return 0;
 }

4e4d293e7b35f0d6f8da9ff3023a50bbd6d84f8e
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-05-12 10:24:09 +0530

    Update MergeSort.cpp

--- Practical/week4/MergeSort.cpp
+++ Practical/week4/MergeSort.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 using namespace std;
-void merge(int arr[], int l, int m, int h)
+int merge(int arr[], int l, int m, int h,int *cmp)
 {
-    int n1=m-l+1,n2=h-m;
+    int n1=m-l+1,n2=h-m,inv=0;
     int a1[n1];
     int a2[n2];
     for(int i=l,j=0;i<=m;i++,j++)
@@ -17,6 +17,7 @@ void merge(int arr[], int l, int m, int h)
     int i=0,j=0,k=l;
     while(i < n1 && j < n2)
     {
+        (*cmp)++;
         if(a1[i] < a2[j])
         {
             arr[k]=a1[i];
@@ -24,6 +25,7 @@ void merge(int arr[], int l, int m, int h)
         }
         else
         {
+            inv += n1 - i;
             arr[k]=a2[j];
             j++;
         }
@@ -41,18 +43,19 @@ void merge(int arr[], int l, int m, int h)
         j++;
         k++;  
     }
+    return inv;
 }
-void mergesort(int arr[],int l,int h)
+int mergesort(int arr[],int l,int h,int *cmp)
 {
-    if(l==h)
-        return;
-    else
+    int inv=0;
+    if(l < h)
     {
         int mid=(l+h)/2;
-        mergesort(arr,l,mid);
-        mergesort(arr,mid+1,h);
-        merge(arr,l,mid,h);
+        inv+=mergesort(arr,l,mid,&(*cmp));
+        inv+=mergesort(arr,mid+1,h,&(*cmp));
+        inv+=merge(arr,l,mid,h,&(*cmp));
     }
+    return inv;
 }
 void readArr(int a[],int n)
 {
@@ -71,13 +74,17 @@ void printArr(int a[],int n)
 }
 int main()
 {
-    int n;
-    cout<<"Enter Size of array:";
-    cin>>n;
-    int arr[n];
-    readArr(arr,n);
-    printArr(arr,n);
-    mergesort(arr,0,n-1);
-    printArr(arr,n);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,inver,cmp=0;
+        cin>>n;
+        int arr[n];
+        readArr(arr,n);
+        inver=mergesort(arr,0,n-1,&cmp);
+        printArr(arr,n);
+        cout<<"Inversion :- "<<inver<<endl;
+        cout<<"Comparison :- "<<cmp<<endl;
+    }
     return 0;
 }

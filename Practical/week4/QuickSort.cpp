33ea2e9edb1eb43a20708f09a749f0706b388832
Ayush Kumar <94746640+AyushKumar170621@users.noreply.github.com>
2022-05-12 10:24:58 +0530

    Update QuickSort.cpp

--- Practical/week4/QuickSort.cpp
+++ Practical/week4/QuickSort.cpp
@@ -1,3 +1,5 @@
+/*2. Given an unsorted array of integers, design an algorithm and implement it using a program to sort an array of elements by partitioning the array into two subarrays based on a pivot element such that one of the sub array holds values smaller than the pivot element while another sub array holds values greater than the pivot element. Pivot element should be selected randomly from the array. Your program should also find number of comparisons and swaps required for sorting the array.
+Source code : -*/
 #include <iostream>
 
 using namespace std;
@@ -7,30 +9,33 @@ void swap(int &a,int &b)
     a=b;
     b=temp;
 }
-int partition(int arr[],int l,int h)
+int partition(int arr[],int l,int h,int *sp,int *cp)
 {
-    int x=arr[l];
-    int i=l;
-    for(int j=l+1;j<= h;j++)
+    int pivot = arr[h];
+    int i = (l - 1); 
+    for (int j = l; j <= h - 1; j++)
     {
-        if(arr[j] <= x)
+        (*cp)++;
+        if (arr[j] < pivot)
         {
-            i++;
-            swap(arr[i],arr[j]);
+            i++; 
+            (*sp)++;
+            swap(arr[i], arr[j]);
         }
     }
- 
-    swap(arr[l],arr[i]);
-    return i;
+    swap(arr[i + 1], arr[h]);
+    (*sp)++;
+    return (i + 1);
+    
 }
-void quicksort(int arr[],int l,int h)
+void quicksort(int arr[],int l,int h,int *sp,int *cp)
 {
     if(l  < h)
     
     {
-        int par=partition(arr,l,h);
-        quicksort(arr,l,par-1);
-        quicksort(arr,par+1,h);
+        int par=partition(arr,l,h,&(*sp),&(*cp));
+        quicksort(arr,l,par-1,&(*sp),&(*cp));
+        quicksort(arr,par+1,h,&(*sp),&(*cp));
     }
 }
 void readArr(int a[],int n)
@@ -50,13 +55,19 @@ void printArr(int a[],int n)
 }
 int main()
 {
-    int n;
-    cout<<"Enter Size of array:";
-    cin>>n;
-    int arr[n];
-    readArr(arr,n);
-    printArr(arr,n);
-    quicksort(arr,0,n-1);
-    printArr(arr,n);
+    int t;
+    cin>>t;
+    while(t--){
+        int n,cmp=0,sw=0;
+        cout<<"Enter Size of array:";
+        cin>>n;
+        int arr[n];
+        readArr(arr,n);
+        quicksort(arr,0,n-1,&sw,&cmp);
+        printArr(arr,n);
+        cout<<"Comparison :- "<<cmp<<endl;
+        cout<<"Swap :- "<<sw<<endl;
+
+    }
     return 0;
 }
